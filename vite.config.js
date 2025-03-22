bdf38db12cb9a283119bd66bc7d1a8ebf710c842
Maico Gabriel Zurbriggen <zurbriggenmaico8@gmail.com>
2025-03-22 16:35:45 -0300

    Pagina de contacto realizada

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Portfolio_Daryl_Dixon/',
   build: { 
     rollupOptions: { 
+      input: {
+        main: 'index.html',
+        historia: 'historia.html',
+        habilidades: 'habilidades.html',
+        hazanias: 'hazanias.html',
+        contacto: 'contacto.html',
+      },
       output: { 
         entryFileNames: '[name].js', 
         chunkFileNames: '[name].js', 
